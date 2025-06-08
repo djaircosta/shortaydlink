01b000de71f2a91335503e1cbc5429f3c7977f34
Djair Costa <riajd10@gmail.com>
2025-06-08 13:00:00 -0300

    feat: Project created

---
+++ backend/src/main/java/br/com/aydconnection/shortaydlink/ShortaydlinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.aydconnection.shortaydlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortaydlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortaydlinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/aydconnection/shortaydlink/ShortaydlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aydconnection.shortaydlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortaydlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
